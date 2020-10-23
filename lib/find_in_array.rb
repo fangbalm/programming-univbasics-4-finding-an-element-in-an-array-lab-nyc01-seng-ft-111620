3658c2610aeef8ddd3cf915d981f614b6bc279bc
Lucius VanSlyke <lu.vanslyke@gmail.com>
2020-10-23 01:46:52 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -3,8 +3,7 @@ def find_element_index(array, value_to_find)
   count = 0
   while count < array.length do
     if (array[count] == value_to_find)
-      return array[count]
-    else
-      return nil
+      return count
     end
+    count = count + 1 
 end
